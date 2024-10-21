2b6c0164dd82679a83404d947ea350eeb814e8bd
MadVij2021 <harshitchopra2003@gmail.com>
2024-10-21 21:48:15 +0530

    HEY

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,11 +1,57 @@
 import streamlit as st
+# import mediapipe as mpp
+# import cv2
+# import mediapipe as mp
+# from mediapipe.tasks import python
+# from mediapipe.tasks.python import vision
+# from mediapipe import solutions
+# from mediapipe.framework.formats import landmark_pb2
+# import numpy as np
 
+
+# MODEL_PATH="D:\SIH 2024\pose_landmarker_heavy.task"
+# def draw_landmarks_on_image(rgb_image, detection_result):
+#   pose_landmarks_list = detection_result.pose_landmarks
+#   annotated_image = np.copy(rgb_image)
+
+#   # Loop through the detected poses to visualize.
+#   for idx in range(len(pose_landmarks_list)):
+#     pose_landmarks = pose_landmarks_list[idx]
+
+#     # Draw the pose landmarks.
+#     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+#     pose_landmarks_proto.landmark.extend([
+#       landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+#     ])
+#     solutions.drawing_utils.draw_landmarks(
+#       annotated_image,
+#       pose_landmarks_proto,
+#       solutions.pose.POSE_CONNECTIONS,
+#       solutions.drawing_styles.get_default_pose_landmarks_style())
+#   return annotated_image
+
+# # STEP 2: Create an PoseLandmarker object.
+# base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
+# options = vision.PoseLandmarkerOptions(
+#     base_options=base_options,
+#     output_segmentation_masks=True)
+# detector = vision.PoseLandmarker.create_from_options(options)
+
+# # STEP 3: Load the input image.
+# image = mp.Image.create_from_file("image.jpg")
+
+# # STEP 4: Detect pose landmarks from the input image.
+# detection_result = detector.detect(image)
+
+# # STEP 5: Process the detection result. In this case, visualize it.
+# annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+# cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
 
 enable = st.checkbox("Hows it?")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
-if picture:
-    st.image(picture)
+# if picture:
+#     st.image(picture)
 
 
 st.write("Hello World!!!, Lets say whether you will update changes or not in this app")
