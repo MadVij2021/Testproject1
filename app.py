2f7f5d4f49a811fa403deeeeb94f28ca786c5579
MadVij2021 <harshitchopra2003@gmail.com>
2024-11-05 23:44:32 +0530

    commit4

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av
+from sample_utils.turn import get_ice_servers
+import cv2
 
 st.title("Webcam App with Streamlit WebRTC")
 
@@ -11,16 +13,14 @@ RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.goog
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
     # (Optional) Process the frame (e.g., apply filters)
-    
+    img=cv2.flip(img,0)
     # Return the processed frame
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 # WebRTC streamer component
 webrtc_streamer(
-    key="webcam",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration=RTC_CONFIGURATION,
-    media_stream_constraints={"video": True, "audio": False},
-    video_frame_callback=video_frame_callback,
-    async_processing=True,
+    key="programatic_control",
+    rtc_configuration={  # Add this line
+        "iceServers": get_ice_servers()
+    }
 )
