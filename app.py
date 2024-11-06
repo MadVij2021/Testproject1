eb998128623ba30482939d43b7009e34e9adb09f
MadVij2021 <harshitchopra2003@gmail.com>
2024-11-06 22:50:42 +0530

    app changes

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration, VideoTransformerBase
 import av
 from sample_utils.turn import get_ice_servers
 import cv2
 
-st.title("Webcam App with Streamlit WebRTC")
+st.title("KAISA APP HAI YEH BSDK")
 
 # RTC Configuration for the video stream
 RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 # Function to process each video frame
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    # (Optional) Process the frame (e.g., apply filters)
-    img=cv2.flip(img,0)
-    # Return the processed frame
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+class VideoTransforms(VideoTransformerBase):
+    def transform(self,frame):
+        img = frame.to_ndarray(format="bgr24")
+        # (Optional) Process the frame (e.g., apply filters)
+        img=cv2.flip(img,1)
+        # Return the processed frame
+        return img
 
 # WebRTC streamer component
-webrtc_streamer(
-    key="programatic_control",
-    rtc_configuration={  # Add this line
-        "iceServers": get_ice_servers()
-    }
-)
+webrtc_streamer(key="example",video_transformer_factory=VideoTransforms)
